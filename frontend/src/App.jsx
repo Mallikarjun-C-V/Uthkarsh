ddf82052fe48a000f98274998811ab7b8daeff6c
Mallikarjun C V <mallikarjuncv12@gmail.com>
2025-03-20 13:44:53 +0530

    mallu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <p className="text-red-800">Malli</p>
+      <p className="text-red-800">utkarsh</p>
     </div>
   );
 };
